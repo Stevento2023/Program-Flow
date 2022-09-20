f8da57f069c38a9c32f8e33627f556d16d141a47
Steven To <steven.to@mrjaffesclass.com>
2022-09-20 11:41:21 -0700

    9/20 If Statement

--- ProgramFlow.java
+++ ProgramFlow.java
@@ -1,33 +1,40 @@
-
-/**
- * Write a description of class ProgramFlow here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class ProgramFlow
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class ProgramFlow
-     */
-    public ProgramFlow()
-    {
-        // initialise instance variables
-        x = 0;
+    
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        if (a == 5) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if (b == 2) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if (c != 1) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if (d == -8 && a == 4) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+        
+    public static void main(String[] args)
     {
-        // put your code here
-        return x + y;
+        ProgramFlow hw = new ProgramFlow();
+        hw.run();
     }
 }
